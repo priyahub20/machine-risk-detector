8432057cf5a5c9401bd3f2cebf49aa32b5340ee4
Mohana priya R <mohanapriya162020@gmail.com>
2026-07-05 09:12:38 +0530

    Remove base path from Vite configuration
    Removed the base path configuration for the project.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/machine-risk-detector/',
-
   plugins: [
     react(),
     babel({
